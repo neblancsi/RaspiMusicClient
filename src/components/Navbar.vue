151c9045e7a099959f8c3faa850fb719a07adb20
Nébl Annamária <neblancsi95@gmail.com>
2021-06-06 12:07:35 +0200

    sticky nav

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,25 +1,23 @@
 <template>
-  <div>
-    <b-navbar toggleable="lg" type="dark" variant="red">
-      <b-navbar-brand to="/">
-        <img
-          src="https://cdn.iconscout.com/icon/free/png-256/raspberry-pi-3-569254.png"
-          width="30px"
-          height="30px"
-        />
-      </b-navbar-brand>
+  <b-navbar toggleable="lg" type="dark" variant="dark" sticky="true">
+    <b-navbar-brand to="/">
+      <img
+        src="https://cdn.iconscout.com/icon/free/png-256/raspberry-pi-3-569254.png"
+        width="30px"
+        height="30px"
+      />
+    </b-navbar-brand>
 
-      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
-      <b-collapse id="nav-collapse" is-nav>
-        <b-navbar-nav>
-          <b-nav-item to="/">Keresés</b-nav-item>
-          <b-nav-item to="/controller">Lejátszás</b-nav-item>
-          <b-nav-item to="/raspi">Pi</b-nav-item>
-        </b-navbar-nav>
-      </b-collapse>
-    </b-navbar>
-  </div>
+    <b-collapse id="nav-collapse" is-nav>
+      <b-navbar-nav>
+        <b-nav-item to="/">Keresés</b-nav-item>
+        <b-nav-item to="/controller">Lejátszás</b-nav-item>
+        <b-nav-item to="/raspi">Pi</b-nav-item>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
 </template>
 
 <script>
